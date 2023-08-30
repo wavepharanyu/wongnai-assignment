5d5136cc2fdb169359835eb83a11d2c1d02d8f51
pharanyuwave@gmail.com <pharanyuwave@gmail.com>
2023-08-31 03:45:05 +0700

    feature: add open/close status

--- packages/web/src/components/header/Header.tsx
+++ packages/web/src/components/header/Header.tsx
@@ -1,20 +1,42 @@
 import { Link, useNavigate } from 'react-router-dom'
+import moment from 'moment'
 import './Header.scss'
 
-const Header = (props: {name : string}) => {
+const Header = (props: {name : string, open : string, close: string}) => {
+    const {name, open, close} = props
+    const calIsOpen = () => {
+        let openHours = parseInt(open.substring(0,2) , 10)
+        let closeHours = parseInt(close.substring(0,2) , 10)
+        let nowHours = moment().hour()
+
+        let openMin = parseInt(close.substring(3,5) , 10)
+        let closeMin = parseInt(close.substring(3,5) , 10)
+        let nowMin = moment().minute()
+
+        if((nowHours >= openHours) && (nowHours <= closeHours) && (nowMin >=  openMin) && (nowMin <=  closeMin)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
   return (
     <div className='header-container'>
-        <Link to='/' className='header-link'>
-            <div className='header-group'>
+        <div className='header-group'>
+            <Link to='/' className='header-link'>
                 <div className='header-logo'>
                     <img className='logo' src="/images/cup.png" alt="logo"/>
                 </div>
                 <div className='header-name'>
-                    <h1 className='text'>{props.name}</h1>
+                    <h1 className='text'>{name}</h1>
+                    
                 </div>
-            </div>
-        </Link>
-       
+            </Link>
+        </div>
+        <div className={calIsOpen() ? 'header-open' : 'header-close'}>
+            <h3 className='text'>{calIsOpen() ? 'เปิด' : 'ปิด'}</h3>
+        </div>
     </div>
   )
 }

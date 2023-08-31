6ad219ed32f7bb6714592e40e2442c2eaf0827e6
pharanyuwave@gmail.com <pharanyuwave@gmail.com>
2023-08-31 13:57:24 +0700

    edit: แก้ status เปิด/ปิด ร้าน

--- packages/web/src/components/header/Header.tsx
+++ packages/web/src/components/header/Header.tsx
@@ -1,20 +1,39 @@
 import { Link, useNavigate } from 'react-router-dom'
+import moment from 'moment'
 import './Header.scss'
 
-const Header = (props: {name : string}) => {
+const Header = (props: {name : string, open : string, close: string}) => {
+    const {name, open, close} = props
+    const calIsOpen = () => {
+        const format = 'hh:mm'
+
+        const openTime = moment(open, format)
+        const closeTime = moment(close, format)
+
+        if(moment().isBetween(openTime, closeTime)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
   return (
     <div className='header-container'>
-        <Link to='/' className='header-link'>
-            <div className='header-group'>
+        <div className='header-group'>
+            <Link to='/' className='header-link'>
                 <div className='header-logo'>
                     <img className='logo' src="/images/cup.png" alt="logo"/>
                 </div>
                 <div className='header-name'>
-                    <h1 className='text'>{props.name}</h1>
+                    <h1 className='text'>{name}</h1>
+                    
                 </div>
-            </div>
-        </Link>
-       
+            </Link>
+        </div>
+        <div className={calIsOpen() ? 'header-open' : 'header-close'}>
+            <h3 className='text'>{calIsOpen() ? 'เปิด' : 'ปิด'}</h3>
+        </div>
     </div>
   )
 }
